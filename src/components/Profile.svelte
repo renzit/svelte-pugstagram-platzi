c7c1f9a42d6404b21bd24cc26cd09ae032810df2
Renzo <renzo.mayer@vix.com>
2020-07-12 16:35:29 -0300

    initial structure and components

---
+++ src/components/Profile.svelte
@@ -0,0 +1,11 @@
+<div class="Profile">
+    <div class="Profile-content">
+        <div class="Profile-avatar">
+            <img src="https://arepa.s3.amazonaws.com/oscar.png" alt="">
+        </div>
+        <div class="Profile-info">
+            <h2>renzit</h2>
+            <span>Renzo Mayer</span>
+        </div>
+    </div>
+</div>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,14 @@
+<script>
+import Profile from './Profile.svelte';
+import Stories from './Stories.svelte';
+import Footer from './Footer.svelte';
+</script>
+
+
+<div class="Sidebar">
+    <div class="Sidebar-container">
+        <Profile />
+        <Stories />
+        <Footer />
+    </div>
+</div>
