59e0ebf13780a6004046ec977528f9cfb86b20d4
Renzo <renzo.mayer@vix.com>
2020-07-17 21:30:58 -0300

    add functionality and style

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,16 @@
 <script>
-  let actualYear = newDate().getFullYear();
+  let actualDate = new Date();
+  let actualYear = actualDate.getFullYear();
 </script>
+
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
 
 <div class="Footer">
   <div class="Footer-container">
--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,19 +1,37 @@
 <script>
+    import { onMount } from "svelte";
     import Header from '../components/Header.svelte';
     import Main from '../components/Main.svelte';
     import Sidebar from '../components/Sidebar.svelte';
     import TimeLine from '../components/TimeLine.svelte';
+
+    let data = {};
+    const API = "https://us-central1-pugstagram-co.cloudfunctions.net/data";
+
+    onMount(async() => {
+        const response = await fetch(API)
+        data = await response.json();
+    })
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
     :global(body) {
-        background-color: #676778;
-        color:white;
+        background-color: #fafafa;
+        color:rgba(38,38,38, 0.7);
+        font-family: "Lato", sans-serif;
+        margin:0;
+        padding:0;
+    }
+    :global(h1, h2, h3, h4, h5, h6){
+        margin:0;
+        padding:0;
     }
 </style>
 
 <Header />
 <Main>
-    <TimeLine />
-    <Sidebar />
+    <TimeLine posts={data.posts} />
+    <Sidebar {...data.user} />
 </Main>
